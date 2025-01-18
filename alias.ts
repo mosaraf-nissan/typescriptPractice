e3513c52c9bdddba373625dbe3cd951417df59ed
Md. Mosaraf Hossain Nissan <117709837+mosaraf-nissan@users.noreply.github.com>
2025-01-19 00:45:30 +0600

    functionAlias

--- alias.ts
+++ alias.ts
@@ -5,7 +5,7 @@
     name: string;
     age: number;
     gender: string;
-    contactNo: string;
+    contactNo?: string;
     address: string;
   };
 
@@ -15,5 +15,18 @@
     gender: "male",
     contactNo: "1234567890",
     address: "Bangalore",
+  };
+
+  const student2: Student = {
+    name: "Nissan",
+    age: 30,
+    gender: "male",
+    address: "Bangalore",
+  };
+
+  //   type of alias for function
+  type AddFunction = (a: number, b: number) => number;
+  const add: AddFunction = (a, b) => {
+    return a + b;
   };
 }
