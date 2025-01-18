6d52650d8ef53423fdc03584f53d838663b35bf4
Md. Mosaraf Hossain Nissan <117709837+mosaraf-nissan@users.noreply.github.com>
2025-01-19 00:40:01 +0600

    type of alias

---
+++ alias.ts
@@ -0,0 +1,19 @@
+{
+  // type for alias
+
+  type Student = {
+    name: string;
+    age: number;
+    gender: string;
+    contactNo: string;
+    address: string;
+  };
+
+  const student1: Student = {
+    name: "Nissan",
+    age: 30,
+    gender: "male",
+    contactNo: "1234567890",
+    address: "Bangalore",
+  };
+}
--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 // basic data types
-// string
-let firstName: string = "Nissan";
-// number
-let age: number = 30;
-// boolean
-let isStudent: boolean = false;
-//null
-let data: null = null;
+{
+  // string
+  let firstName: string = "Nissan";
+  // number
+  let age: number = 30;
+  // boolean
+  let isStudent: boolean = false;
+  //null
+  let data: null = null;
+}
